d2160764a60ed7c8b50e34221c3e5a35b5d3c1df
GaoXiong <xiong.gao@shopee.com>
2026-01-20 00:33:38 +0800

    chore: dev

--- app/src/main/java/com/example/test_filesync/api/ApiConfig.java
+++ app/src/main/java/com/example/test_filesync/api/ApiConfig.java
@@ -9,4 +9,5 @@ public class ApiConfig {
   public static final String user_userInfo = "/app/account/get_user_info";  // 获取用户信息
   public static final String report_location = "/app/report/report_location";  // 上报位置信息
   public static final String report_installed_app = "/app/report/report_installed_app";  // 上报已安装应用列表
+  public static final String report_screenshot = "/app/report/report_screenshot";  // 上报截图
 }
